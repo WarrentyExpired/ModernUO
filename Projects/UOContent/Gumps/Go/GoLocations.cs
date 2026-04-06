8326a6e4fff30b2c5283c9534630c3c43374ca2f
WarrentyExpired <warrentyexpired@gmail.com>
2026-04-06 19:14:52 -0400

    # 'Go' Menu now only reads Data/Locatoins/world.json file.

--- Projects/UOContent/Gumps/Go/GoLocations.cs
+++ Projects/UOContent/Gumps/Go/GoLocations.cs
@@ -11,47 +11,17 @@ public static class GoLocations
 {
     private static readonly ILogger logger = LogFactory.GetLogger(typeof(GoLocations));
 
-    private static LocationTree _felucca;
-    private static LocationTree _trammel;
-    private static LocationTree _ilshenar;
-    private static LocationTree _malas;
-    private static LocationTree _tokuno;
-    private static LocationTree _terMur;
+    private static LocationTree _world;
 
     public static LocationTree GetLocations(Map map)
     {
-        if (map == Map.Ilshenar)
-        {
-            return _ilshenar ??= LoadLocations("ilshenar", Map.Ilshenar);
-        }
-
-        if (map == Map.TerMur)
-        {
-            return _terMur ??= LoadLocations("termur", Map.TerMur);
-        }
-
-        if (map == Map.Trammel)
-        {
-            return _trammel ??= LoadLocations("trammel", Map.Trammel);
-        }
-
-        if (map == Map.Malas)
-        {
-            return _malas ??= LoadLocations("malas", Map.Malas);
-        }
-
-        if (map == Map.Tokuno)
-        {
-            return _tokuno ??= LoadLocations("tokuno", Map.Tokuno);
-        }
-
-        return _felucca ??= LoadLocations("felucca", Map.Felucca);
+        // Always return the same world tree, regardless of which map 'GetLocations' is called with
+        return _world ??= LoadLocations("world", Map.Felucca);
     }
 
     private static LocationTree LoadLocations(string fileName, Map map)
     {
         var lastBranch = new Dictionary<Mobile, GoCategory>();
-
         var path = Path.Combine($"Data/Locations/{fileName}.json");
 
         try
@@ -67,7 +37,7 @@ public static class GoLocations
         catch (Exception e)
         {
             logger.Error(e, "Failed to load file {Path}.", path);
-            return new LocationTree(map, [], null);
+            return new LocationTree(map, lastBranch, new GoCategory { Name = "Error", Categories = [], Locations = [] });
         }
     }
 }
