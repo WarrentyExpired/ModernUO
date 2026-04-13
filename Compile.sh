123d6dba8b270390978d742d7db829e646f3b572
WarrentyExpired <warrentyexpired@gmail.com>
2026-04-13 15:32:03 -0400

    # Committed to the wrong branch.

--- Compile.sh
+++
@@ -1,1 +0,0 @@
-dotnet publish Projects/Application/Application.csproj -c Release -r linux-x64 --no-restore --self-contained=false
--- Run.sh
+++
@@ -1,3 +0,0 @@
-echo "Starting Server."
-dotnet ./Distribution/ModernUO.dll
-
